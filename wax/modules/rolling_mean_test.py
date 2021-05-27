49ee48a405606d61cefc9d6748c047f10331bef4
Emmanuel Sérié <eserie@gmail.com>
2021-05-27 09:13:05 +0200

    [wax.modules.RollingMean] rename the 'horizon' parameter to 'window', add the 'min_periods' parameter, manage nan values, test against pandas

--- wax/modules/rolling_mean_test.py
+++ wax/modules/rolling_mean_test.py
@@ -13,28 +13,55 @@
 # limitations under the License.
 import haiku as hk
 import jax
+import jax.numpy as jnp
+import pandas as pd
+from jax.config import config
 
+from wax.compile import jit_init_apply
 from wax.modules.rolling_mean import RollingMean
+from wax.unroll import dynamic_unroll
 
 
-def test_rolling_mean():
+def test_rolling_mean_init_apply():
+    @jit_init_apply
     @hk.transform_with_state
-    def mean(x):
+    def rolling_mean(x):
         return RollingMean(2)(x)
 
     seq = hk.PRNGSequence(42)
     x = jax.random.normal(next(seq), (2, 3))
-    params, state = mean.init(next(seq), x)
-    output, state = mean.apply(params, state, next(seq), x)
+    params, state = rolling_mean.init(next(seq), x)
+    output, state = rolling_mean.apply(params, state, next(seq), x)
     assert output is not x
     assert (output == x).all()
     x1 = x
 
     x = jax.random.normal(next(seq), (2, 3))
-    output, state = mean.apply(params, state, next(seq), x)
+    output, state = rolling_mean.apply(params, state, next(seq), x)
     assert ((x1 + x) / 2 == output).all()
     x2 = x
 
     x = jax.random.normal(next(seq), (2, 3))
-    output, state = mean.apply(params, state, next(seq), x)
+    output, state = rolling_mean.apply(params, state, next(seq), x)
     assert ((x2 + x) / 2 == output).all()
+
+
+def test_run_ema_vs_pandas_not_adjust(window=10, min_periods=5):
+
+    config.update("jax_enable_x64", True)
+
+    seq = hk.PRNGSequence(42)
+    x = jax.random.normal(shape=(100, 5), key=next(seq), dtype=jnp.float64)
+    mask = jax.random.choice(next(seq), 2, shape=x.shape)
+    x = jnp.where(mask, x, jnp.nan)
+
+    @jit_init_apply
+    @hk.transform_with_state
+    def rolling_mean(x):
+        return RollingMean(window, min_periods)(x)
+
+    mean, state = dynamic_unroll(rolling_mean, x, key=next(seq))
+
+    mean_pandas = pd.DataFrame(x).rolling(window, min_periods).mean()
+
+    assert jnp.allclose(mean, mean_pandas.values, equal_nan=True)

45bd038fd115f738ebca52a03a2b12bf791549e0
Emmanuel Sérié <eserie@gmail.com>
2021-06-03 15:14:12 +0200

    format notebook

--- docs/notebooks/04_The_three_steps_workflow.py
+++ docs/notebooks/04_The_three_steps_workflow.py
@@ -14,22 +14,25 @@
 #     name: python3
 # ---
 
-# +
+# + id="2b295407-92c4-4818-bfb9-f445f6967f10"
 # Uncomment to run the notebook in Colab
 # # ! pip install -q "wax-ml[complete]@git+https://github.com/eserie/wax-ml.git"
 # # ! pip install -q --upgrade jax jaxlib==0.1.67+cuda111 -f https://storage.googleapis.com/jax-releases/jax_releases.html
-# -
 
+# + id="ff30291d"
 # check available devices
 import jax
 
+# + id="a3cdb104" outputId="2a8b84e5-1d90-4c06-bed5-1518bdfdc767" colab={"base_uri": "https://localhost:8080/"}
 print("jax backend {}".format(jax.lib.xla_bridge.get_backend().platform))
 jax.devices()
 
+# + [markdown] id="1fa1808c"
 # # 🎛 The 3-steps workflow 🎛
 #
 # [![Open in Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/eserie/wax-ml/blob/main/docs/notebooks/04_The_three_steps_workflow.ipynb)
 
+# + [markdown] id="2e1eefec"
 # It is already very useful to be able to execute a JAX function on a dataframe in a single work step
 # and with a single command line thanks to WAX-ML accessors.
 #
@@ -54,9 +57,10 @@ jax.devices()
 #
 # Let's illustrate how to reimplement WAX-ML EWMA yourself with the WAX-ML 3-step workflow.
 
+# + [markdown] id="bd2e906e"
 # ## Imports
 
-# + tags=[]
+# + tags=[] id="2bdfbf9a"
 import haiku as hk
 import numpy as onp
 import pandas as pd
@@ -71,66 +75,75 @@ from wax.stream import tree_access_data
 from wax.unroll import dynamic_unroll
 
 register_wax_accessors()
-# -
 
+# + [markdown] id="a0999c69"
 # ## Performance on big dataframes
 
+# + [markdown] id="6f8447b4"
 # ### Generate data
 
-# + tags=["parameters"]
+# + tags=["parameters"] id="768d7802-580d-4c31-9a0e-e6dc4f0589ca"
 T = 1.0e5
 N = 1000
 
-# + tags=[]
+# + tags=[] id="03af743d" outputId="ae3f7e8f-7e2c-4ef1-a456-bd4d2f72893d" colab={"base_uri": "https://localhost:8080/"}
 # %%time
 T, N = map(int, (T, N))
 dataframe = pd.DataFrame(
     onp.random.normal(size=(T, N)), index=pd.date_range("1970", periods=T, freq="s")
 )
-# -
 
+# + [markdown] id="d1fd46f7"
 # ### Pandas EWMA
 
-# + tags=[]
+# + tags=[] id="27092faf" outputId="760f16e5-bc4d-4bf1-fa1b-7749e1d2366f" colab={"base_uri": "https://localhost:8080/"}
 # %%time
 df_ewma_pandas = dataframe.ewm(alpha=1.0 / 10.0).mean()
-# -
 
+# + [markdown] id="678be283"
 # ### WAX-ML EWMA
 
-# + tags=[]
+# + tags=[] id="11f3705d" outputId="75921f06-a92c-4e1a-a7f9-49866659c232" colab={"base_uri": "https://localhost:8080/"}
 # %%time
 df_ewma_wax = dataframe.wax.ewm(alpha=1.0 / 10.0).mean()
-# -
 
+# + [markdown] id="0d94d5cf"
 # It's a little faster, but not that much faster...
 
+# + [markdown] id="e51ee290"
 # ### WAX-ML EWMA (without format step)
 
+# + [markdown] id="7361def9"
 # Let's disable the final formatting step (the output is now in raw JAX format):
 
-# + tags=[]
+# + tags=[] id="f87f5668" outputId="7e3e0052-803a-48ac-ae9a-c2b68af8c575" colab={"base_uri": "https://localhost:8080/"}
 # %%time
 df_ewma_wax_no_format = dataframe.wax.ewm(alpha=1.0 / 10.0, format_outputs=False).mean()
-# -
 
+# + id="88d0cab5-62ad-47f7-9d06-56fc45fa542e" outputId="41fe33ca-4713-4a9d-a595-5c85696ff76f" colab={"base_uri": "https://localhost:8080/"}
 type(df_ewma_wax_no_format)
 
+# + [markdown] id="9be62475-b2fa-4a95-b293-7e4410ca36ca"
 # Let's check the device on which the calculation was performed (if you have GPU available, this should be `GpuDevice` otherwise it will be `CpuDevice`):
 
+# + id="38d3970c-04a5-4deb-93b3-a4f5f899e8f1" outputId="ae302feb-5e16-4a53-8aad-6dd12de7d4e8" colab={"base_uri": "https://localhost:8080/"}
 df_ewma_wax_no_format.device()
 
+# + [markdown] id="784ee16e"
 # That's better! In fact (see below)
 # there is a performance problem in the final formatting step.
 # See WEP3 for a proposal to improve the formatting step.
 
+# + [markdown] id="c5e7b817"
 # ### Generate data (in dataset format)
 #
 # WAX-ML `Sream` object works on datasets.
 # So let's transform the `DataFrame` into a xarray `Dataset`:
 
+# + id="9965444b"
 dataset = xr.DataArray(dataframe).to_dataset(name="dataarray")
 
+# + [markdown] id="123965eb"
 # ## Step (1) (synchronize | data tracing | encode)
 #
 # In this step,  WAX-ML do:
@@ -145,37 +158,49 @@ dataset = xr.DataArray(dataframe).to_dataset(name="dataarray")
 # It prepares a function that wraps the input function with the actual data and indices
 # in a pair of pure functions (`TransformedWithState` Haiku tuple).
 
+# + id="5f6b72ca" outputId="18f8761e-f198-476c-a3e0-06b6773d874a" colab={"base_uri": "https://localhost:8080/"}
 # %%time
 stream = dataframe.wax.stream()
 
+# + [markdown] id="4cc34aad-3e15-4220-9dff-30dcad660307"
 # Define our custom function to be applied on a dict of arrays
 # having the same structure than the original dataset:
 
 
+# + id="2bfa2a6c"
 def my_ewma_on_dataset(dataset):
     return EWMA(alpha=1.0 / 10.0, adjust=True)(dataset["dataarray"])
 
 
+# + id="4735903f"
 transform_dataset, jxs = stream.prepare(dataset, my_ewma_on_dataset)
 
+# + [markdown] id="18b31346-501a-407b-b586-780117d043f3"
 # Let's definite the init parameters and state of the transformation we
 # will apply.
 
+# + [markdown] id="78ea9eda-d0cc-4dba-8b2a-60fbf1dd41bd"
 # ### Init params and state
 
+# + id="47b1ebdf-ff32-4c2a-ae0b-51b88882328b"
 from wax.unroll import init_params_state
 
+# + id="ea6651f6-0db8-48f0-b578-013b2cb74272"
 rng = jax.random.PRNGKey(42)
 params, state = init_params_state(transform_dataset, rng, jxs)
 
+# + id="dc9a4bce-83d1-494b-b053-c6f9ebfb7d0c" outputId="4c4e08a8-5c03-468f-96bb-c0b10fdb1d79" colab={"base_uri": "https://localhost:8080/"}
 params
 
+# + id="4dd83bef-ef68-4576-973f-594f18123944"
 assert state["ewma"]["count"].shape == (N,)
 assert state["ewma"]["mean"].shape == (N,)
 
 
+# + [markdown] id="903a778c"
 # ## Step (2) (compile | code tracing | execution)
 
+# + [markdown] id="425830f4"
 # In this step we:
 # - prepare a pure function (with
 #   [Haiku's transform mechanism](https://dm-haiku.readthedocs.io/en/latest/api.html#haiku-transforms))
@@ -187,28 +212,34 @@ assert state["ewma"]["mean"].shape == (N,)
 # - perform code tracing and execution (the last line):
 #     - Unroll the transformation on "steps" `xs` (a `np.arange` vector).
 
+# + id="6825fdc8-1773-4e04-a41c-a126a1527891"
 rng = next(hk.PRNGSequence(42))
 outputs, state = dynamic_unroll(transform_dataset, params, state, rng, False, jxs)
 
+# + id="3862a486-a5a2-4aa9-967a-59ebc32a18e1" outputId="bb926b58-cb8e-44df-a38d-56e6fa758466" colab={"base_uri": "https://localhost:8080/"}
 outputs.device()
 
+# + [markdown] id="b73f3252"
 # Once it has been compiled and "traced" by JAX, the function is much faster to execute:
 
-# + tags=[]
+# + tags=[] id="a889d294" outputId="1bbcad84-5bc0-49d3-865f-685e75892793" colab={"base_uri": "https://localhost:8080/"}
 # %%timeit
 outputs, _ = dynamic_unroll(transform_dataset, params, state, rng, False, jxs)
-# -
 
+# + id="f19185cd-15c2-4c9c-bbff-8d50deb1fee2" outputId="b32b7d89-570d-4b09-ac53-d1119144ee65" colab={"base_uri": "https://localhost:8080/"}
 # %%time
 outputs, _ = dynamic_unroll(transform_dataset, params, state, rng, False, jxs)
 
+# + [markdown] id="987e8b63"
 # This is 3x faster than pandas implementation!
 #
 # (The 3x factor is obtained by measuring the execution with %timeit.
 # We don't know why, but when executing a code cell once at a time, then the execution time can vary a lot and we can observe some executions with a speed-up of 100x).
 
+# + [markdown] id="4c8acec1-4414-4340-9cfc-199e90565d4d"
 # ### Manually prepare the data and manage the device
 
+# + [markdown] id="8b76775b-76f7-45ae-8bc7-66a6f945370f"
 # In order to manage the device on which the computations take place,
 # we need to have even more control over the execution flow.
 # Instead of calling `stream.prepare` to build the `transform_dataset` function,
@@ -217,12 +248,14 @@ outputs, _ = dynamic_unroll(transform_dataset, params, state, rng, False, jxs)
 # - converting the numpy data in jax ourself
 # - puting the data on the device we want.
 
+# + id="76479f76-7b6e-4321-97c3-942f2987bc01" outputId="c4c46c35-4e93-4f7b-8197-0520f8f0e1e0" colab={"base_uri": "https://localhost:8080/"}
 np_data, np_index, xs = stream.trace_dataset(dataset)
 jnp_data, jnp_index, jxs = convert_to_tensors((np_data, np_index, xs), "jax")
 
+# + [markdown] id="a4fa1934-3633-4661-b808-c60e4b8c4600"
 # We explicitly set data on CPUs (the is not needed if you only have CPUs):
 
-# +
+# + id="bae591f7-e7ec-4b8b-9b80-6f129184e1ae" outputId="f073bab3-7f7b-4ed6-a5df-5266418d4072" colab={"base_uri": "https://localhost:8080/"}
 from jax.tree_util import tree_leaves, tree_map
 
 cpus = jax.devices("cpu")
@@ -232,13 +265,14 @@ jnp_data, jnp_index, jxs = tree_map(
 print("data copied to CPU device.")
 
 
-# -
-
+# + [markdown] id="f851fbe7-096e-4399-ae5c-08739837dfeb"
 # We have now "JAX-ready" data for later fast access.
 
+# + [markdown] id="19f3b58e-61f3-481c-a512-cb87bde622a8"
 # Let's define the transformation that wrap the actual data and indices in a pair of
 # pure functions:
 
+# + id="e7ebbb08-d790-4977-b49d-c9224e299a42" outputId="c91ac27c-b671-4bad-8e0d-c9eb0464ae3c" colab={"base_uri": "https://localhost:8080/"}
 # %%time
 @jit_init_apply
 @hk.transform_with_state
@@ -247,27 +281,36 @@ def transform_dataset(step):
     return EWMA(alpha=1.0 / 10.0, adjust=True)(dataset["dataarray"])
 
 
+# + [markdown] id="31fa1d05-c3d7-4543-ac82-d398044cdc2e"
 # And we can call it as before:
 
+# + id="f851407a-597f-4711-8370-c3e83cb50da7" outputId="585097e9-6025-4f45-886e-261cbdba4014" colab={"base_uri": "https://localhost:8080/"}
 # %%time
 outputs, state = dynamic_unroll(transform_dataset, None, None, rng, False, jxs)
 
+# + id="e5b786b7-ecaf-4280-894b-aa8f65a0b78f" outputId="c8bc9f41-7587-4482-c496-f40523f6840f" colab={"base_uri": "https://localhost:8080/"}
 outputs.device()
 
+# + [markdown] id="6fa51498"
 # ## Step(3) (format)
 # Let's come back to pandas/xarray:
 
+# + id="d1b0732c" outputId="7427109b-7cfa-455c-af20-080b73b86182" colab={"base_uri": "https://localhost:8080/"}
 # %%time
 y = format_dataframe(
     dataset.coords, onp.array(outputs), format_dims=dataset.dataarray.dims
 )
 
+# + [markdown] id="e27bd3b8"
 # It's quite slow (see WEP3 enhancement proposal).
 
+# + [markdown] id="3cdc9281-6092-4368-a0e8-ed26a5114106"
 # ## GPU execution
 
+# + [markdown] id="f0d4651d-9087-4f04-9f07-a4d92cd3ba1f"
 # Let's look with execution on GPU
 
+# + id="a30f6024-5e9c-4174-92ea-7207860d829d" outputId="ee0cc84f-e332-485b-fddd-ee5ce42b6ce8" colab={"base_uri": "https://localhost:8080/"}
 try:
     gpus = jax.devices("gpu")
     jnp_data, jnp_index, jxs = tree_map(
@@ -279,22 +322,29 @@ except RuntimeError as err:
     print(err)
     GPU_AVAILABLE = False
 
+# + [markdown] id="27d7cf26-f35b-489e-83b9-c120565d9b17"
 # Let's check that our data is on the GPUs:
 
+# + id="d6f00e5d-1b85-483d-9856-35de3a954b13" outputId="db9396c3-b35d-4cdc-aaa3-eadddc9da31c" colab={"base_uri": "https://localhost:8080/"}
 tree_leaves(jnp_data)[0].device()
 
+# + id="6f89fc7c-4824-4858-aee8-6fff7834c70c" outputId="40c8a645-5771-40f0-ffab-0e5322d59c77" colab={"base_uri": "https://localhost:8080/"}
 tree_leaves(jnp_index)[0].device()
 
+# + id="ba2471ef-51e4-49f2-863c-aabafd401cbf" outputId="25800a2f-4152-4714-9ceb-ba396fcf1790" colab={"base_uri": "https://localhost:8080/"}
 jxs.device()
 
+# + id="6c35bd5d-0113-455d-bd56-f7c31bf6c736" outputId="86e2ff5d-3b7b-4b65-8202-0feebc27063d" colab={"base_uri": "https://localhost:8080/"}
 # %%time
 if GPU_AVAILABLE:
     rng = next(hk.PRNGSequence(42))
     outputs, state = dynamic_unroll(transform_dataset, None, None, rng, False, jxs)
 
 
+# + [markdown] id="274b5615-8785-43e7-a2db-5f867566c913"
 # Let's redefine our function `transform_dataset` by explicitly specify to `jax.jit` the `device` option.
 
+# + id="6bb7431e-90f2-4761-a906-69f25fea4a63" outputId="405cc724-22e8-48d6-e722-d6cea8a90e49" colab={"base_uri": "https://localhost:8080/"}
 # %%time
 if GPU_AVAILABLE:
 
@@ -310,8 +360,10 @@ if GPU_AVAILABLE:
     rng = next(hk.PRNGSequence(42))
     outputs, state = dynamic_unroll(transform_dataset, None, None, rng, False, jxs)
 
+# + id="944dcb8e-211c-4b39-b854-12118fe775ed" outputId="d1f353b2-a7ed-4457-a26c-38ac4125aa65" colab={"base_uri": "https://localhost:8080/"}
 outputs.device()
 
+# + id="de99ee71-4e02-4843-9e6e-39d831f9697e" outputId="88a5e604-a722-43bf-d90c-49b11fde96b2" colab={"base_uri": "https://localhost:8080/"}
 # %%timeit
 if GPU_AVAILABLE:
     outputs, state = dynamic_unroll(transform_dataset, None, None, rng, False, jxs)

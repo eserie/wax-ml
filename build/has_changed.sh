d80a8713aca332d0cbf3c3e4e3d82ed5b015af45
Emmanuel Sérié <eserie@gmail.com>
2021-10-18 22:51:00 +0200

    correct script

--- build/has_changed.sh
+++ build/has_changed.sh
@@ -3,7 +3,7 @@
 if [ $(git status --porcelain | wc -l) -eq "0" ]; then
   echo "  🟢 Git repo is clean."
 else
-  echo "  🔴 Git repo dirty. Quit.
+  echo "  🔴 Git repo dirty. Quit."
   git status
   exit 1
 fi

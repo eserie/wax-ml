2b1be6ec5155f419c974f14cf56bf82797250c4d
Emmanuel Sérié <eserie@gmail.com>
2021-10-19 11:21:46 +0200

    update Makefile and docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -200,6 +200,7 @@ execution_excludepatterns = [
     # Slow notebook: execution on big dataframes
     'notebooks/04_The_three_steps_workflow.*',
     'notebooks/05_reconstructing_the_light_curve_of_stars.*'
+    'notebooks/07_Online_Time_Series_Prediction.*'
 ]
 
 # -- Options for HTMLHelp output ---------------------------------------------

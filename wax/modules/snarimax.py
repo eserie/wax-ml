2b1006ee50cfc0fc134e85304ce8df306ff0775b
Emmanuel Sérié <eserie@gmail.com>
2021-10-20 10:05:44 +0200

    fix flake8

--- wax/modules/snarimax.py
+++ wax/modules/snarimax.py
@@ -88,7 +88,7 @@ class SNARIMAX(hk.Module):
         self.regressor = regressor if regressor is not None else hk.Linear(1)
         if d != 0 or sp != 0 or sd != 0 or sq != 0:
             raise NotImplementedError(
-                f"Options 'd', 'sp', 'sd', 'sq' are not yet implemented."
+                "Options 'd', 'sp', 'sd', 'sq' are not yet implemented."
             )
 
     def __call__(self, y, X=None):

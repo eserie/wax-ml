439265045f8aea9f7438093295da9e30fbab6b79
Emmanuel Sérié <eserie@gmail.com>
2021-12-05 22:23:19 +0100

    fix mypy

--- wax/modules/online_supervised_learner.py
+++ wax/modules/online_supervised_learner.py
@@ -73,7 +73,7 @@ class OnlineSupervisedLearner(hk.Module):
         params, state = hk.get_state(
             "model_params_state",
             [],
-            init=lambda *_: ParamsState(*self.model.init(hk.next_rng_key(), x)),
+            init=lambda *_: ParamsState(*self.model.init(hk.next_rng_key(), x)),  # type: ignore
         )
         opt_state = hk.get_state("opt_state", [], init=lambda *_: self.opt.init(params))
 

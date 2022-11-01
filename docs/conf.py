0d7c8369aa91d63f791b386560bb3440613c7270
Emmanuel Sérié <eserie@gmail.com>
2022-11-01 11:37:00 +0100

    set docs parameter language='en'

--- docs/conf.py
+++ docs/conf.py
@@ -95,7 +95,7 @@ templates_path = ['_templates']
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # The suffix(es) of source filenames.
 # Note: important to list ipynb before md here: we have both md and ipynb

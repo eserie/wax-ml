cc3a67b5e72159e0fb1e834abd4ed65fdc7eed82
Emmanuel Sérié <eserie@gmail.com>
2022-11-14 09:21:33 +0100

    update

--- wax/stateful.py
+++ wax/stateful.py
@@ -14,36 +14,60 @@
 from typing import Callable
 
 import haiku as hk
-from haiku.experimental import lift_with_state
 import jax
+from haiku.experimental import lift_with_state
 from jax.tree_util import tree_map
 
-def vmap_lift_with_state(fun: Callable, split_rng=False, init_rng=False):
+
+def vmap_lift_with_state(fun: Callable, split_rng=False):
     def apply_fn(*args, **kwargs):
 
-        tfun = hk.transform_with_state(fun)    
-        n_batches = len(jax.tree_util.tree_leaves((args, kwargs))[0])
-        if hk.running_init() and init_rng:
+        tfun = hk.transform_with_state(fun)
+
+        if not split_rng:
+            try:
+                init_rng = hk.next_rng_key() if hk.running_init() else None
+            except ValueError:
+                init_rng = None
+
+            @jax.vmap
+            def init_fn(*args, **kwargs):
+                return tfun.init(init_rng, *args, **kwargs)
+
+            params_and_state_fn, updater = hk.experimental.lift_with_state(
+                init_fn, name="f_lift", allow_reuse=False
+            )
+            params, state = params_and_state_fn(*args, **kwargs)
+
+            @jax.vmap
+            def apply_fn(params, state, *args, **kwargs):
+                return tfun.apply(params, state, None, *args, **kwargs)
+
+            out, state = apply_fn(params, state, *args, **kwargs)
+        else:
+            n_batches = len(jax.tree_util.tree_leaves((args, kwargs))[0])
+            if hk.running_init():
+                rng = hk.next_rng_key()
+                rng = jax.random.split(rng, num=n_batches)
+            else:
+                rng = None
+
+            @jax.vmap
+            def init_fn(rng, *args, **kwargs):
+                return tfun.init(rng, *args, **kwargs)
+
+            params_and_state_fn, updater = hk.experimental.lift_with_state(
+                init_fn, name="f_lift", allow_reuse=False
+            )
+            params, state = params_and_state_fn(rng, *args, **kwargs)
+
+            @jax.vmap
+            def apply_fn(params, state, rng, *args, **kwargs):
+                return tfun.apply(params, state, rng, *args, **kwargs)
+
             rng = hk.next_rng_key()
             rng = jax.random.split(rng, num=n_batches)
-        else:
-            rng = None
-
-        @jax.vmap
-        def init_fn(rng, *args, **kwargs):
-            return tfun.init(rng, *args, **kwargs)
-
-        params_and_state_fn, updater = lift_with_state(
-            init_fn, name="f_lift", allow_reuse=False
-        )
-        params, state = params_and_state_fn(rng, *args, **kwargs)
-
-        if split_rng:
-            rng = hk.next_rng_key()
-            rng = jax.random.split(rng, num=n_batches)
-        else:
-            rng = None
-        out, state = jax.vmap(apply_fn, in_axes=[(0,), (0,), (0 if split_rng else None)])(params, state, rng, *args, **kwargs)
+            out, state = apply_fn(params, state, rng, *args, **kwargs)
 
         updater.update(state)
         return out
